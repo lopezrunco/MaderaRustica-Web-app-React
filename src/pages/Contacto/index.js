38ed0c4047de5c43f4eae864a1dfc6eb023548d0
lopezrunco <lopezrunco@gmail.com>
2021-11-09 18:49:25 -0300

    ContactInfo component

--- src/pages/Contacto/index.js
+++ src/pages/Contacto/index.js
@@ -1,4 +1,5 @@
 import PageTitle from "../../components/PageTitle"
+import ContactInfo from "../../components/ContactInfo"
 
 function Contacto() {
     return (
@@ -7,6 +8,14 @@ function Contacto() {
                 <div className="row">
                     <div className="col">
                         <PageTitle title="Contáctenos y haremos el trabajo a su medida" subtitle="Contacto" />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-5">
+                        <ContactInfo />
+                    </div>
+                    <div className="col-md-7">
+
                     </div>
                 </div>
             </div>
