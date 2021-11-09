13f6b8c9affe4d564b766b3f3be5ae9da283f3ce
lopezrunco <lopezrunco@gmail.com>
2021-11-09 20:05:15 -0300

    News page component

--- src/pages/Novedades/index.js
+++ src/pages/Novedades/index.js
@@ -1,4 +1,9 @@
 import PageTitle from "../../components/PageTitle"
+import NewsItem from "../../components/NewsItem"
+
+import casasImage from '../../assets/img/casas.jpg'
+import maderaDuraImage from '../../assets/img/madera-dura.jpg'
+import carpinterosImage from '../../assets/img/carpinteros.jpg'
 
 function Novedades() {
     return (
@@ -6,8 +11,13 @@ function Novedades() {
             <div className="container">
                 <div className="row">
                     <div className="col">
-                        <PageTitle title="Últimas noticias de nuestro rubro" subtitle="Novedades" />
+                        <PageTitle title="Últimas noticias del rubro" subtitle="Novedades" />
                     </div>
+                </div>
+                <div className="row news-grid">
+                    <NewsItem title="Cada vez más gente elige construir en madera" category="Arquitectura" bgImage={casasImage} />
+                    <NewsItem title="Madera contaminada en Guichón" category="Medio ambiente" bgImage={maderaDuraImage} />
+                    <NewsItem title="Los carpinteros son una especie en extinción" category="Interés general" bgImage={carpinterosImage} />
                 </div>
             </div>
         </main>
