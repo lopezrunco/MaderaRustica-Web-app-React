734758c02d51b15438843080e20f231ef697aaf9
lopezrunco <lopezrunco@gmail.com>
2021-11-28 17:23:57 -0300

    Dinamic data in Services page

--- src/components/ServicesGrid/index.js
+++ src/components/ServicesGrid/index.js
@@ -1,39 +1,22 @@
 import ServiceItem from "../ServicesItem"
+import { servicesArray } from '../../data'
 
 function ServicesGrid() {
     return (
         <div className="services-grid">
             <div className="row">
-                <ServiceItem 
-                    iconClassName="fas fa-battery-full" 
-                    title="Rendimiento" 
-                    text="Evaluar la durabilidad y el rendimiento de los productos para cumplir con las expectativas de los clientes." 
-                />
-                <ServiceItem 
-                    iconClassName="fas fa-globe-americas" 
-                    title="Medioambiente" 
-                    text="Reduccir el impacto medioambiental mientras garantizamos resultados positivos para las personas." 
-                />
-                <ServiceItem 
-                    iconClassName="fas fa-burn" 
-                    title="Inflamabilidad" 
-                    text="Determinar la resistencia al fuego de los muebles, tejidos para fundas y materiales de relleno." 
-                />
-                <ServiceItem 
-                    iconClassName="fas fa-comment-dots" 
-                    title="Consultoría" 
-                    text="Solicite nuestra experiencia para favorecer cualquier aspecto de su negocio." 
-                />
-                <ServiceItem 
-                    iconClassName="fas fa-plug" 
-                    title="Seguridad eléctrica" 
-                    text="Asegurar que los productos de mobiliario cumplan con los estándares de seguridad." 
-                />
-                <ServiceItem 
-                    iconClassName="fas fa-handshake" 
-                    title="Mantenimiento" 
-                    text="Llegado el momento, despreocúpse por el mantenimiento. Nosotros lo hacemos por usted." 
-                />
+
+                {/* Returns a ServiceItem component for every object on the array */}
+                {servicesArray.map(({ iconClassName, title, text, id }) => (
+
+                    <ServiceItem
+                        key={id}
+                        iconClassName={iconClassName}
+                        title={title}
+                        text={text}
+                    />
+
+                ))}
             </div>
         </div>
     )
---
+++ src/data/index.js
@@ -0,0 +1,9 @@
+// Services page data
+export const servicesArray = [
+    { iconClassName: 'fas fa-battery-full', title: 'Rendimiento', text: 'Evaluar la durabilidad y el rendimiento de los productos para cumplir con las expectativas de los clientes.', id: '0' },
+    { iconClassName: 'fas fa-globe-americas', title: 'Medioambiente', text: 'Reduccir el impacto medioambiental mientras garantizamos resultados positivos para las personas..', id: '1' },
+    { iconClassName: 'fas fa-burn', title: 'Inflamabilidad', text: 'Determinar la resistencia al fuego de los muebles, tejidos para fundas y materiales de relleno.', id: '2' },
+    { iconClassName: 'fas fa-comment-dots', title: 'Consultoría', text: 'Solicite nuestra experiencia para favorecer cualquier aspecto de su negocio.', id: '3' },
+    { iconClassName: 'fas fa-plug', title: 'Seguridad eléctrica', text: 'Asegurar que los productos de mobiliario cumplan con los estándares de seguridad.', id: '4' },
+    { iconClassName: 'fas fa-handshake', title: 'Mantenimiento', text: 'Llegado el momento, despreocúpse por el mantenimiento. Nosotros lo hacemos por usted.', id: '5' }
+]
